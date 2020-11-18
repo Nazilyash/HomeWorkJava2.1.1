cf202383d87559311bdbf0fa9e835e5c775092e1
misharka <fareshta88@gmail.com>
2020-11-18 12:03:45 +0300

    Main.java modified

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       int TicketPriceInKopecks = 999999;
-       int Tariff = 20;
-       int AviaMiles = TicketPriceInKopecks / Tariff / 100;
-        System.out.println("Количество начисленных миль за купленный билет (когда учитыватся только целая часть значения миль):");
-        System.out.println(AviaMiles);
-        System.out.println("Done");
-
-        float TicketPrice = 9999.99F;
-        float Tariff1 = 20;
-        float AviaMiles1 = TicketPrice / Tariff1;
-        System.out.println("Количество начисленных миль за купленный билет (когда учитыватся дробная часть значения миль):");
-        System.out.println(AviaMiles1);
+       int ticketPriceInKopecks = 999999;
+       int tariff = 20;
+       int aviaMiles = ticketPriceInKopecks / tariff / 100;
+        System.out.println("Количество начисленных миль за купленный билет:");
+        System.out.println(aviaMiles);
         System.out.println("Done");
     }
 }
